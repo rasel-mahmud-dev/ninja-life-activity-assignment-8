70db7abf0b1383f87a515a5a6a4b0fba1f4e9b14
rasel mahmud dev <rasel.mahmud.dev@gmail.com>
2022-09-28 20:53:41 +0600

    added activity page and json data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header/Header";
+import ActivityList from "./pages/ActivityList";
 
 
 function App() {
   return (
     <div className="container">
       
-      <Header />
+        <Header />
+        <ActivityList />
     
     </div>
   );
--- src/pages/ActivityList.js
+++ src/pages/ActivityList.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import "./activityList.css"
 
 const ActivityList = () => {
+	const [activities, setActivities] = useState([])
+	
+	useEffect(()=>{
+		fetch("/data.json")
+			.then(res=>res.json())
+			.then(data=>{
+			setActivities(data)
+			})
+			.catch(ex=>{
+			
+			})
+	}, [])
+	
 	return (
-		<div>
-			<h4>Select Daily Activity</h4>
+		<div className="activity-container">
+			<h4 className="section-title">Select Daily Activity</h4>
+			{ activities.map(activity=>(
+				<div>
+					<h1>{activity.title}</h1>
+				</div>
+			)) }
+			
         </div>
 	);
 };
