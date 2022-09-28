644a0a86a378189accda88795762d4e834d4fb41
rasel mahmud dev <rasel.mahmud.dev@gmail.com>
2022-09-28 20:39:37 +0600

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./header.css";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<div className='logo'>
+				<img src="/logo.jpg" alt=""/>
+				<h4>Ninja-Life-Activity</h4>
+			</div>
+        </header>
+	);
+};
+
+export default Header;
---
+++ src/pages/ActivityList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ActivityList = () => {
+	return (
+		<div>
+			<h4>Select Daily Activity</h4>
+        </div>
+	);
+};
+
+export default ActivityList;
