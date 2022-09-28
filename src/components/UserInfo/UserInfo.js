b4c410f4df2c5ba098dd9507bb5480e2d5e5f3f8
rasel mahmud dev <rasel.mahmud.dev@gmail.com>
2022-09-28 22:28:52 +0600

    added user info component and apply some styles

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
 
 const UserInfo = (props) => {
-	return (
-		<div>
-			<img src="" alt=""/>
-			<div>
-				<h2>Rasel Mahmud</h2>
-				<p>Bogra Bangladesh</p>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="user-data">
+        <div className="user-photo">
+          <img  src="https://res.cloudinary.com/dbuvg9h1e/image/upload/v1660078654/netflix/images/1660078488473.jpg" alt="" />
+        </div>
+        <div className="">
+          <h2 className="user-name">Rasel Mahmud</h2>
+          <p className="user-address">Bogra Bangladesh</p>
+        </div>
+      </div>
+
+      <div className="user-slats">
+        <span className="slats-item">
+          <div>
+            <span className="slats-number">75</span>
+            <span className="text-small small-slats-unit">kg</span>
+          </div>
+          <span className="slats-unit">Weight</span>
+        </span>
+        <span className="slats-item">
+          <div>
+            <span className="slats-number">6.5</span>
+          </div>
+          <span className="slats-unit">Height</span>
+        </span>
+
+        <span className="slats-item">
+          <div>
+            <span className="slats-number">25</span>
+            <span className="text-small small-slats-unit">yrs</span>
+          </div>
+          <div className="slats-unit">Age</div>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default UserInfo;
